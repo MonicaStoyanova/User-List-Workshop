27898748bc2b3604997b4068030f41b5b1e5283c
MonicaStoyanova <114282401+MonicaStoyanova@users.noreply.github.com>
2023-05-25 13:48:58 +0300

    services
    new component and services are added

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import * as userService from "./services/userService";
 import { Fragment } from "react";
 // we put fragment since it satisfies React needs for parent component without the need to wrap the component in div
 //since in the browser when we inspect it wil appear as nested div in the DOM tree
@@ -10,13 +12,26 @@ import "./App.css";
 import { UserList } from "./components/UserList";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    userService
+      .getAll()
+      .then((users) => {
+        // or we can write: .then(setUsers)
+        setUsers(users);
+      })
+      .catch((err) => {
+        console.log(["Error" + err]);
+      });
+  }, []);
+
   return (
     <Fragment>
       <Header />
       <main className="main">
-        <section class="card users-container">
+        <section className="card users-container">
           <Search />
-          <UserList />
+          <UserList users={users} />
         </section>
       </main>
       <Footer />
